23d7308a502091004370e78af1a077bfeaeac1f5
JCamyre <jwcamry03@gmail.com>
2023-05-18 18:27:47 -0700

    Change lambda

--- lambda.js
+++ lambda.js
@@ -1,4 +1,5 @@
 exports.handler = function (event, context) {
   console.log(event);
-  context.succeed("Yo what's up: ", event);
+  const name = event['name']
+  context.succeed("Yo what's up: ", name);
 };
